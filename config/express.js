a8d3c16ab20b035e882de872f92790528ffd5d4a
ArthurSMA <arthursuassunamaiaalves@gmail.com>
2023-10-23 09:16:25 -0300

    master: alternado rota para não necessitar do require

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
-const express    = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const config     = require('config');
+const config = require('config');
+const consign = require('consign');
 
 module.exports = () => {
   const app = express();
@@ -11,7 +12,11 @@ module.exports = () => {
   // MIDDLEWARES
   app.use(bodyParser.json());
 
-  require('../api/routes/customerWallets')(app);
+  consign({ cwd: 'api' })
+    .then('data')
+    .then('controllers')
+    .then('routes')
+    .into(app);
 
   return app;
 };
